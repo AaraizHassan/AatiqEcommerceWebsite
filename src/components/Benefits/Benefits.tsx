ad37df9f729f06f6229c3e140554940cacf0c4ae
AaraizHassan <113174720+AaraizHassan@users.noreply.github.com>
2026-01-24 19:50:12 +0500

    Image cropping

--- src/components/Benefits/Benefits.tsx
+++ src/components/Benefits/Benefits.tsx
@@ -29,15 +29,22 @@ const Benefits: React.FC = () => {
               height={400}
               className="object-cover w-full h-64"
             /> */}
-            <Image
+            {/* <Image
               src={product.imageSrc}
               alt={product.title}
               width={400}
               height={400}
               sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
               className="object-cover w-full h-64"
+            /> */}
+            <Image
+              src={product.imageSrc}
+              alt={product.title}
+              width={400}
+              height={400}
+              sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+              className="object-contain w-full h-64 bg-gray-100"
             />
-
             <div className="p-4">
               <h3 className="font-semibold text-lg">{product.title}</h3>
               <p className="text-sm text-foreground-accent mt-1">
